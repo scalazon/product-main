de9733f839d4924930df737152e05d3764f4870b
zonagilreath <bryson.gilreath@gmail.com>
2019-07-09 15:39:27 -0500

    implement REST api for product details

---
+++ database/index.js
@@ -0,0 +1,62 @@
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const {dbURI} = require('../config.js');
+
+const conn = mongoose.connect(dbURI, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const ProductSchema = new Schema({
+  asin: String,
+  productTitle: String,
+  bulletPoints: Array,
+  price: Number,
+  category: String,
+  attributes: String,
+  totalImages: Number,
+  imgURLs: Array
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+const essentialProps = ['asin', 'productTitle', 'bulletPoints', 'price', 'category', 'totalImages'];
+
+module.exports.get = (asin) => (
+  Product.find({asin}).limit(1)
+  .then(product => JSON.stringify(product))
+  .catch(console.error)
+)
+
+module.exports.getAll = () => (
+  Product.find({})
+  .then(products => JSON.stringify(products))
+  .catch(console.error)
+);
+
+module.exports.add = (product) => {
+  console.log(product);
+  if (essentialProps.every((prop) => product.hasOwnProperty(prop))) {
+   // if (essentialProps.every(product.hasOwnProperty)) { //why doesn't this work?
+    if (!Array.isArray(product.bulletPoints)){
+      product.bulletPoints = product.bulletPoints.split('\n');
+    }
+    if (product.price[0] === '$'){
+      product.price = product.price.slice(1);
+    }
+    product.imgURLs = [];
+    for (let i = 1; i <= product.totalImages; i++){
+      product.imgURLs.push(`${product.asin}_${i}.jpg`);
+    }
+    return Product(product)
+          .save()
+          .then(prod => {
+            console.log('added:', JSON.stringify(prod))
+            return prod;
+          })
+          .catch(console.error)
+  } 
+}
+
+module.exports.addBatch = (JSONarray) => {
+  return Promise.all(JSONarray.map(prod => this.add(prod)))
+}
--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const prods = require(path.resolve(__dirname, '../database/index.js'));
 
 app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 
+app.get('/products/:asin', (req, res) => {
+  prods.get(req.params.asin)
+  .then(product => res.send(product))
+  .catch(console.error)
+});
+
+app.get('/products', (req, res) => {
+  prods.getAll()
+  .then(products => res.send(products))
+  .catch(console.error)
+});
+
 app.listen(PORT, ()=>{
-  console.log('baby, i\'m listening...');
+  console.log('baby, i\'m listening...', PORT);
 })
