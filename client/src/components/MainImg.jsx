f46d1b5a878583dcfa9df4777a6cce5a42dbe554
Zona <bryson.gilreath@gmail.com>
2019-07-14 09:41:00 -0500

    can't test, don't have my config file

--- client/src/components/MainImg.jsx
+++ client/src/components/MainImg.jsx
@@ -1,2 +1,26 @@
 import React, {Component} from 'react';
 import ReactImageMagnify from 'react-image-magnify';
+
+export default class BasicExample extends Component {
+  render() {
+    return (
+      <div className="fluid__image-container">
+        <ReactImageMagnify {...{
+          smallImage: {
+              isFluidWidth: true,
+              src: `https://hackmazon-images.s3.amazonaws.com/Images/${props.mainImg}`,
+              lensStyle: {
+                background: 'hsla(0, 0%, 100%, .3)',
+                border: '1px solid #ccc'
+              }
+          },
+          largeImage: {
+            src: `https://hackmazon-images.s3.amazonaws.com/Images/${props.mainImg}`,
+            width: 1200,
+            height: 1800
+          }
+        }} />
+      </div>
+    );
+  }
+}
