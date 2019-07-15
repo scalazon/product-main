5e900d9162adad063f74f3856f73be625569f965
zonagilreath <bryson.gilreath@gmail.com>
2019-07-15 10:35:51 -0500

    make bundle name specific to service

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: 'product_main_bundle.js',
     path: DIST_DIR
   },
   module : {
