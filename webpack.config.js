fe03f4c4e819b636c69b454c25a9ed2535053cb7
zonagilreath <bryson.gilreath@gmail.com>
2019-07-20 15:24:26 -0500

    undid some minor changes, added font import

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
