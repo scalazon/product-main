b61c2d4ced3780b5db8c0311f77456de682f5a76
Brendan <bpdavitt@gmail.com>
2019-08-01 11:36:24 -0500

    Add files for using postgreSQL; modify server and testing as needed to account for differences

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
-const prods = require(path.resolve(__dirname, '../database/index.js'));
+// const db = require(path.resolve(__dirname, '../database/index.js'));
+const db = require(path.resolve(__dirname, '../database/pgindex.js'));
+
 
 app.use(compression());
 app.use(cors());
@@ -13,30 +15,48 @@ app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 
 app.get('/products', (req, res) => {
   console.log('Getting some items:')
-  prods.getSome()
+  db.getSome()
     .then(products => res.json(products))
     .catch(console.error)
 });
+
+// *****************  For mongoDB return **************
+// app.get('/products/random', (req, res) => {
+//   let asin = Math.floor(Math.random() * 10000000) + 1;
+//   // let asin = 1500000;
+//   console.log('Getting a random item:', asin)
+//   db.get(asin)
+//     .then(products => res.json(products[0]))
+//     .catch(console.error)
+// });
+
+
+// app.get('/products/:asin', (req, res) => {
+//   console.log('getting a specific product')
+//   db.get(req.params.asin)
+//     .then(products => res.json(products[0]))
+//     .catch(console.error)
+// });   *************** End MongoDB fx *************
 
 app.get('/products/random', (req, res) => {
   let asin = Math.floor(Math.random() * 10000000) + 1;
   // let asin = 1500000;
   console.log('Getting a random item:', asin)
-  prods.get(asin)
-    .then(products => res.json(products[0]))
+  db.get(asin)
+    .then(product => res.json(product))
     .catch(console.error)
 });
 
 
 app.get('/products/:asin', (req, res) => {
   console.log('getting a specific product')
-  prods.get(req.params.asin)
-    .then(products => res.json(products[0]))
+  db.get(req.params.asin)
+    .then(product => res.json(product))
     .catch(console.error)
 });
 
 app.post('/products/:asin', (req, res) => {
-  prods.add(req.body)
+  db.add(req.body)
     .then((data) => {
       console.log('products/:asin', data)
       res.send('Product has been added');
@@ -48,7 +68,7 @@ app.post('/products/:asin', (req, res) => {
 // Then is failing => Research
 app.post('/products', (req, res) => {
   //  console.log(req.body)
-  let promises = prods.addBatch(req.body);
+  let promises = db.addBatch(req.body);
   // console.log(promises);
   Promise.all(promises)
     .then(result => res.send('Products added'))
